5d1674363639d9cfe0fb5e00bca9068e9e288137
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2023-05-14 20:45:58 -0500

    Setup Interface and Application User BLL

---
+++ BLL/ApplicationUserBLL.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SD_340_W22SD_Final_Project_Group6.Data;
+using SD_340_W22SD_Final_Project_Group6.Models;
+using System.Data;
+
+namespace SD_340_W22SD_Final_Project_Group6.BLL
+{
+    public class ApplicationUserBLL
+    {
+		private readonly UserManager<ApplicationUser> _users;
+		private IRepository<ApplicationUser> _applicationUserRepo;
+
+        public ApplicationUserBLL(IRepository<ApplicationUser> applicationUserRepo, UserManager<ApplicationUser> users)
+        {
+            _applicationUserRepo = applicationUserRepo;
+			_users = users;
+		}
+
+        public ICollection<ApplicationUser> GetUsers()
+        {
+            return _applicationUserRepo.GetAll();
+        }
+
+        public async Task AssignUserRole(string role, string userId)
+        {
+			ApplicationUser user = _users.Users.First(u => u.Id == userId);
+
+			ICollection<string> userRoles = await _users.GetRolesAsync(user);
+			if (userRoles.Count == 0)
+			{
+				await _users.AddToRoleAsync(user, role);				
+			}
+			else
+			{
+				await _users.RemoveFromRoleAsync(user, userRoles.First());
+				await _users.AddToRoleAsync(user, role);
+			}
+		}
+    }
+}
---
+++ Data/ApplicationUserRepository.cs
@@ -0,0 +1,39 @@
+﻿using SD_340_W22SD_Final_Project_Group6.Models;
+
+namespace SD_340_W22SD_Final_Project_Group6.Data
+{
+    public class ApplicationUserRepository : IRepository<ApplicationUser>
+    {
+        private ApplicationDbContext _context;
+
+        public ApplicationUserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<ApplicationUser> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public ApplicationUser? GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ApplicationUser entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
